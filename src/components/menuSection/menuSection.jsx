f3e2ad7f4bda3ccdb27499b77722b2b203752616
TheNinza <niks.a3198@gmail.com>
2021-11-10 13:51:07 +0530

    added lower case folders

---
+++ src/components/menuSection/menuSection.jsx
@@ -0,0 +1,43 @@
+import { LinkedButton, MenuSectionContainer } from "./menuSection.styles";
+import { ReactComponent as EncryptionIcon } from "../../assets/encryptIcon.svg";
+import { ReactComponent as DecryptionIcon } from "../../assets/decryptIcon.svg";
+
+const MenuSection = () => {
+  const MenuSectionContainerVariants = {
+    visible: {
+      opacity: 1,
+      translateY: "-50%",
+      translateX: "-50%",
+      transition: {
+        duration: 1.5,
+        type: "spring",
+        stiffness: 50,
+        damping: 5,
+        bounce: 0.5,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      translateY: "-200%",
+      translateX: "-50%",
+    },
+  };
+
+  return (
+    <MenuSectionContainer
+      variants={MenuSectionContainerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+    >
+      <LinkedButton to="/encrypt" content="Encrypt">
+        <EncryptionIcon />
+      </LinkedButton>
+      <LinkedButton to="#" content="Decrypt">
+        <DecryptionIcon />
+      </LinkedButton>
+    </MenuSectionContainer>
+  );
+};
+
+export default MenuSection;
---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,15 @@
+import { HomepageContainer } from "./homepage.styles";
+
+import MenuSection from "../../components/menuSection/menuSection";
+
+const Homepage = () => {
+  // refs
+
+  return (
+    <HomepageContainer>
+      <MenuSection />
+    </HomepageContainer>
+  );
+};
+
+export default Homepage;
